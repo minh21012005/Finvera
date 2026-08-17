23e0ca97b08ebf41481d04529b15b93e5e871814
minh21012005 <baminhss@gmail.com>
2026-08-17 09:45:46 +0700

    first commit

---
+++ finvera-be/src/main/java/com/minhnb/finvera_be/FinveraBeApplication.java
@@ -0,0 +1,13 @@
+package com.minhnb.finvera_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinveraBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinveraBeApplication.class, args);
+	}
+
+}
